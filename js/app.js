2104b73e40e0ff2da4959937c11d680a3a49ed16
Chandrasekhar0707 <chandrasekharsamal981@gmail.com>
2026-07-19 00:29:31 +0530

    Music layout modifying update

--- js/app.js
+++ js/app.js
@@ -24,107 +24,6 @@ function secondsToMinutesSeconds(seconds) {
     return `${formattedMinutes}:${formattedSeconds}`
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// async function getSongs(folder) {
-//     currFolder = folder;
-//     let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
-//     let response = await a.text()
-//     let div = document.createElement("div")
-//     div.innerHTML = response;
-//     let as = div.getElementsByTagName("a")
-//     songs = []
-//     for (let index = 0; index < as.length; index++) {
-//         const element = as[index];
-//         if (element.href.endsWith(".mp3")) {
-//             songs.push(element.href.split(`/${folder}/`)[1])
-//         }
-
-//     }
-
-
-
-//     // Show all the songs in the playlist
-//     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
-//     const noSongsMessage = document.querySelector(".noSongsMessage")
-//     songUL.innerHTML = ""
-//     for (const song of songs) {
-//         songUL.innerHTML = songUL.innerHTML + `<li data-track="${song}"><img class = "invert" width = "34" src = "img/music.svg" altv = "">
-//         <div class = "info">
-//             <div> ${song.replaceAll("%20", " ")}</div>
-//         </div>
-//         <div class = "playnow">
-//             <span> Play Now </span>
-//             <img class = "invert library-play" src = "img/play.svg" alt = "">
-//         </div>
-//         </li>`;
-//     }
-//     if (noSongsMessage) {
-//         noSongsMessage.style.display = songs.length > 0 ? 'none' : 'block'
-//     }
-
-//     // Attach event listeners to each song item and its play button
-//     Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
-//         // click on entire item -> play that track
-//         e.addEventListener("click", element => {
-//             const track = e.dataset.track
-//             playMusic(track)
-//             setActiveLibraryItem(e)
-//         })
-
-//         // click on small play button -> toggle play/pause for that track
-//         const playBtn = e.querySelector('.library-play')
-//         if (playBtn) {
-//             playBtn.addEventListener('click', ev => {
-//                 ev.stopPropagation()
-//                 const track = e.dataset.track
-//                 // if this is the current playing track
-//                 if (currentSong.src.endsWith(track)) {
-//                     if (currentSong.paused) {
-//                         currentSong.play()
-//                         play.src = 'img/pause.svg'
-//                         setActiveLibraryItem(e)
-//                     } else {
-//                         currentSong.pause()
-//                         play.src = 'img/play.svg'
-//                         // reset icon for this item
-//                         playBtn.src = 'img/play.svg'
-//                         activeLibraryItem = null
-//                     }
-//                 } else {
-//                     // start playing this new track
-//                     playMusic(track)
-//                     setActiveLibraryItem(e)
-//                 }
-//             })
-//         }
-//     })
-
-//     return songs
-
-// }
-
-
-
-// REPLACING GPT
-
 async function getSongs(folder) {
     currFolder = folder;
 
@@ -217,22 +116,6 @@ async function getSongs(folder) {
     return songs;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const playMusic = (track, pause = false) => {
     if (track) {
         currentSong.src = `/${currFolder}/` + track
@@ -300,102 +183,6 @@ const openHamburgerMenu = () => {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// async function displayAlbums() {
-//     let a = await fetch(`http://127.0.0.1:5500/songs/`)
-//     // let response = await fetch(`/songs/index.json`); // replace
-//     // let folders = await response.json(); // replace
-
-//     let response = await a.text()
-//     let div = document.createElement("div")
-//     div.innerHTML = response;
-//     let anchors = div.getElementsByTagName("a")
-
-//     let cardContainer = document.querySelector(".cardContainer")
-
-//     let array = Array.from(anchors)
-//     for (let index = 0; index < array.length; index++) {
-//         const e = array[index];
-
-
-//         if (e.href.includes("/songs/")) {
-//             let folder = e.href.split("/").slice(-2)[1]
-//             allFolders.push(folder)
-
-//             // Get meta data of the folder
-//             let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
-//             let response = await a.json();
-//             console.log(response)
-
-//             cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
-//                             <div class="play">
-//                                 <img class="card-play" width="34" src="img/card-play.svg" alt="">
-//                             </div>
-//                             <img
-//                                 src="/songs/${folder}/cover.jpg"
-//                                 alt="" />
-//                             <h2>${response.title}</h2>
-//                             <p>${response.description}</p>
-//                         </div>
-// `
-//         }
-
-//     }
-//     Array.from(document.getElementsByClassName("card")).forEach((e, index) => {
-//         e.addEventListener("click", async () => {
-
-//             const folder = e.dataset.folder;
-
-//             songs = await getSongs(`songs/${folder}`);
-
-//             console.log(songs);
-//             playMusic(songs[0]);
-
-//             setActiveCard(e);
-
-//             currentFolderIndex = allFolders.indexOf(folder);
-
-//             openHamburgerMenu();
-
-//         });
-//     })
-// }
-
-
-// REPLACING GPT
 async function displayAlbums() {
     allFolders = [];
 
@@ -490,16 +277,27 @@ async function main() {
         document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
     })
 
-    // when a track ends, advance to next; if it was the last, restart from first
+    // when a track ends, advance to next in the current playlist
     currentSong.addEventListener('ended', () => {
         if (!Array.isArray(songs) || songs.length === 0) return;
-        const currentTrack = currentSong.src.split('/').slice(-1)[0];
-        const index = songs.indexOf(currentTrack);
-        if (index >= 0 && (index + 1) < songs.length) {
-            playMusic(songs[index + 1])
+
+        // get the filename from the audio src and decode it to match entries in `songs`
+        const lastSegment = currentSong.src.split('/').slice(-1)[0] || '';
+        const currentTrackName = decodeURIComponent(lastSegment);
+
+        // try direct lookup, then fallback to a decoded comparison if needed
+        let index = songs.indexOf(currentTrackName);
+        if (index === -1) {
+            index = songs.findIndex(s => decodeURIComponent(s) === currentTrackName || s === currentTrackName);
+        }
+
+        if (index >= 0 && index + 1 < songs.length) {
+            currentTrackIndex = index + 1;
+            playMusic(songs[currentTrackIndex]);
         } else {
-            console.log(songs);
-            playMusic(songs[0])
+            // reached end — wrap to first track
+            currentTrackIndex = 0;
+            playMusic(songs[0]);
         }
     })
 
